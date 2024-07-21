0391423aa6d9a78bf5c2e9eda44930fd2b0c1eaf
quanglinh <quanglinhh2x@.com>
2024-07-21 10:04:31 +0700

    updayter

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=myusername
-export POSTGRES_PASSWORD=mypasssword
-export POSTGRES_HOST=database-1.comb4hoh2nqs.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=mononith
+export POSTGRES_PASSWORD=mononith
+export POSTGRES_HOST=cdr.cgkju2ujn2qt.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=mono123456
+export AWS_BUCKET=mononith-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=testing
+export JWT_SECRET=anysecret
 export URL=http://localhost:8100
